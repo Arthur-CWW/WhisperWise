11f41a6040dbe4b106ccb4e86eeaeb3aeab0fa9f
chun1 <arthur@chunai.dev>
2023-11-29 23:02:18 +1100

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { Content } from "~/components/Content";
 import { RssTable } from "~/components/ui/table";
 import { api } from "~/utils/api";
 import { todo } from "~/utils/todo";
-import { type ColumnDef } from "@tanstack/react-table";
+import { createColumnHelper, type ColumnDef } from "@tanstack/react-table";
 import { Sidebar } from "../components/Sidebar";
 export function HoverBgColor({
   children,
@@ -29,7 +29,6 @@ export function HoverBgColor({
   );
 }
 const Home: NextPage = () => {
-  const [open, setOpen] = useState(false);
   // const [url, setUrl] = useState("");
   const { data } = api.mdParser.getRSSFeed.useQuery(
     { url: "http://www.aaronsw.com/2002/feeds/pgessays.rss" },
@@ -44,79 +43,37 @@ const Home: NextPage = () => {
   type RSSFeed = {
     items: Article[];
   };
+  const columns: ColumnDef<Article>[] = [
+    { accessorKey: "title", header: "Title" },
+    { accessorKey: "link", header: "Link" },
+  ];
   return (
-    <>
-      <Head>
-        <title>Smart Cast</title>
-        <meta
-          name="description"
-          content="Convert text articles to audiobooks or podcast"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <>
-        <header
-          className=" font-baseline flex h-12 w-full
-          justify-center bg-cyan-900  px-1 text-white"
-        >
-          <button className="px-2" onClick={() => setOpen(!open)}>
-            <HiMenu className="text-2xl text-gray-400" />
-          </button>
-          <div
-            className="grid w-full grid-cols-[65%_1fr]
-                        items-center justify-between px-1"
-          >
-            <h1 className="text-bold h-auto truncate text-2xl">
-              {data?.title ?? "Smart Cast"}
-            </h1>
-            <div className="flex justify-end gap-3">
-              <button onClick={todo()}>
-                <HiEye className="text-2xl " />
-              </button>
-              <button onClick={todo()}>
-                <HiRefresh className="text-2xl text-gray-400" />
-              </button>
-              <button onClick={todo()}>
-                <HiBookmark className="text-2xl text-gray-400" />
-              </button>
-            </div>
-          </div>
-        </header>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = e.currentTarget.elements;
+          console.log(`submitted ${e.currentTarget.value}`);
+          // api.mdParser.submitWebsite.mutation({
+          //   text: e.currentTarget.elements["url"].value,
+          // });
+        }}
+      >
+        <label htmlFor="url">Enter a url</label>
+        <input type="text" id="url" />
 
-        <main
-          className={`${
-            open ? "grid-cols-[auto_1fr]" : "grid-cols-1"
-          } grid h-screen w-full`}
-        >
-          {/* <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const formData = e.currentTarget.elements;
-              // console.log(`submitted ${e.currentTarget.value}`);
-              api.mdParser.submitWebsite.mutation({
-                text: e.currentTarget.elements["url"].value,
-              });
-            }}
-          >
-            <label htmlFor="url">Enter a url</label>
-            <input type="text" id="url" />
+        <label htmlFor="md_to_mp3">Enter url for markdown</label>
+        <input type="text" id="md_to_mp3" />
+        <button type="submit">Submit</button>
+      </form>
+      {/* <pre className="overflow-auto whitespace-pre-wrap">
+        {JSON.stringify(data)}
+      </pre> */}
+      <RssTable data={(data?.items as Article[]) ?? []} columns={columns} />
 
-            <label htmlFor="md_to_mp3">Enter url for markdown</label>
-            <input type="text" id="md_to_mp3" />
-            <button type="submit">Submit</button>
-          </form> */}
-          {/* <pre className="overflow-auto whitespace-pre-wrap">
-              {JSON.stringify(data)}
-            </pre> */}
-          {/* <RssTable data={(data?.items as Article[]) ?? []} columns={columns} /> */}
-
-          {open && <Sidebar />}
-
-          {/* {hello.data && <p>{hello.data.greeting}</p>} */}
-          {/* <Content /> */}
-        </main>
-      </>
-    </>
+      {/* {hello.data && <p>{hello.data.greeting}</p>} */}
+      {/* <Content /> */}
+    </div>
   );
 };
 
--- src/pages/liked.tsx
+++ src/pages/liked.tsx
@@ -10,52 +10,7 @@ import { todo } from "~/utils/todo";
 const Home: NextPage = () => {
   const hello = api.mdParser.submitWebsite.useQuery({ text: "from tRPC" });
   const [open, setOpen] = useState(false);
-  return (
-    <>
-      <Head>
-        <title>Smart Cast</title>
-        <meta
-          name="description"
-          content="Convert text articles to audiobooks or podcast"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header
-        className=" font-baseline flex h-12 w-full
-          justify-center bg-cyan-900  px-1 text-white"
-      >
-        <button className="px-2" onClick={() => setOpen(!open)}>
-          <HiMenu className="text-2xl text-gray-400" />
-        </button>
-        <div
-          className="grid w-full grid-cols-[65%_1fr]
-                        items-center justify-between px-1"
-        >
-          <h1 className="text-bold h-auto truncate text-2xl">Likes</h1>
-          <div className="flex justify-end gap-3">
-            <button onClick={todo()}>
-              <HiEye className="text-2xl " />
-            </button>
-            <button onClick={todo()}>
-              <HiRefresh className="text-2xl text-gray-400" />
-            </button>
-            <button onClick={todo()}>
-              <HiBookmark className="text-2xl text-gray-400" />
-            </button>
-          </div>
-        </div>
-      </header>
-
-      <main
-        className={`${
-          open ? "grid-cols-[auto_1fr]" : "grid-cols-1"
-        } grid h-screen w-screen`}
-      >
-        {open && <Sidebar />}
-        <Content />
-      </main>
-    </>
-  );
+  return <div>Todo</div>;
 };
 
 export default Home;
